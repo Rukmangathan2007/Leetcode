f893589eb5e92e5ff1fe413828b1e21f69e05d5a
RUKMA <rukmangatham2007@gmail.com>
2026-08-23 20:29:37 +0530

    Time: 8 ms (53.28%), Space: 47.3 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n=num.length();
+        int sum1=0,sum2=0,q1=0,q2=0;
+
+        for(int i=0;i<n/2;i++){
+            if(num.charAt(i)=='?')
+                q1++;
+            else
+                sum1+=num.charAt(i)-'0';
+        }
+
+        for(int i=n/2;i<n;i++){
+            if(num.charAt(i)=='?')
+                q2++;
+            else
+                sum2+=num.charAt(i)-'0';
+        }
+
+        int diff=sum1-sum2;
+        int q=q1-q2;
+
+        return 2*diff+9*q != 0;
+    }
+}
